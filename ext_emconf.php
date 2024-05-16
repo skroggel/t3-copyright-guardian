81005fd095436a80732171b2c616d1310b3fa995
Steffen Kroggel <kroggel@rkw.de>
2024-05-16 07:26:05 +0200

    [FIX] typo in description

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
 	'title' => 'Copyright Guardian',
-	'description' => 'Manages the copyright details of images and other media and offers a convienient way of displaying these automatically and directly on the page on which the media is used. In addition, a list of all media used and the associated copyright information can be output',
+	'description' => 'Manages the copyright details of images and other media and offers a convenient way of displaying these automatically and directly on the page on which the media is used. In addition, a list of all media used and the associated copyright information can be output',
 	'category' => 'FE',
 	'author' => 'Steffen Kroggel',
 	'author_email' => 'developer@steffenkroggel.de',
